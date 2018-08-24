fe07b1e5ed4955a7cc75a57c2e3716efa227265b
codediodeio <hello@codediode.io>
2018-08-24 16:43:08 -0700

    feat: working ops

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,11 @@
 import { Component } from '@angular/core';
+import {
+  AngularFirestore,
+  AngularFirestoreDocument
+} from 'angularfire2/firestore';
+
+import { map, switchMap, tap } from 'rxjs/operators';
+import { combineLatest, pipe, of } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +13,102 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'baseSimple';
+  user;
+  joined;
+  constructor(private afs: AngularFirestore) {
+    this.user = this.afs
+      .doc('users/jeff')
+      .valueChanges()
+      .pipe(join(afs, { pet: 'pets', bff: 'users', car: 'cars' }));
+
+    this.joined = this.afs
+      .collection('users')
+      .valueChanges()
+      .pipe(
+        join2(afs, 'pet', 'pets'),
+        join2(afs, 'foo', 'bar'),
+        tap(console.log)
+      );
+  }
 }
+
+// Usage:
+// key = property on parent containing related docID
+// val = path to collection with related doc
+
+export const join = (
+  afs: AngularFirestore,
+  paths: { [key: string]: string }
+) => {
+  let parent;
+  const keys = Object.keys(paths);
+
+  return pipe(
+    switchMap(data => {
+      // Save the parent data state
+      parent = data;
+
+      // Map each path to an Observable
+      const docs$ = keys.map(k => {
+        const fullPath = `${paths[k]}/${parent[k]}`;
+        return afs.doc(fullPath).valueChanges();
+      });
+
+      // return combineLatest, it waits for all reads to finish
+      return combineLatest(docs$);
+    }),
+    map(arr => {
+      // We now have all the associated douments
+      // Reduce them to a single object based on the parent's keys
+      const joins = keys.reduce((acc, cur, idx) => {
+        return { ...acc, [cur]: arr[idx] };
+      }, {});
+
+      // Return the parent doc with the joined objects
+      return { ...parent, ...joins };
+    })
+  );
+};
+
+export const join2 = (afs: AngularFirestore, field, collection) => {
+  let collectionData;
+  const cache = new Map();
+
+  return pipe(
+    switchMap(data => {
+      // Save the parent data state
+      collectionData = data as any[];
+
+      const reads$ = [];
+      let i = 0;
+      for (const doc of collectionData) {
+        // Skip if doc field does not exist or is already in cache
+        if (!doc[field] || cache.get(doc[field])) {
+          continue;
+        }
+
+        // Push doc read to Array
+        reads$.push(
+          afs
+            .collection(collection)
+            .doc(doc[field])
+            .valueChanges()
+        );
+        cache.set(doc[field], i);
+        i++;
+      }
+
+      return reads$.length ? combineLatest(reads$) : of([]);
+    }),
+    map(joins => {
+      const final = collectionData.map((v, i) => {
+        const joinIdx = cache.get(v[field]);
+        return { ...v, [field]: joins[joinIdx] || null };
+      });
+
+      console.log(`Query size ${final.length} + joined ${cache.size} docs`);
+
+      return final;
+    })
+  );
+};
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,29 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { AngularFireModule } from 'angularfire2';
+import { AngularFirestoreModule } from 'angularfire2/firestore';
+import { AngularFireAuthModule } from 'angularfire2/auth';
+
+const config = {
+  apiKey: 'AIzaSyBogpc-AUVgVdcgoiWhE4_lKCFzfcbaSaA',
+  authDomain: 'angularfirebase-267db.firebaseapp.com',
+  databaseURL: 'https://angularfirebase-267db.firebaseio.com',
+  projectId: 'angularfirebase-267db',
+  storageBucket: 'angularfirebase-267db.appspot.com',
+  messagingSenderId: '96751198234'
+};
+
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    AngularFireModule.initializeApp(config),
+    AngularFireAuthModule,
+    AngularFirestoreModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
